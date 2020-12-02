1190eef55eb643ca7e66a0b0e83edaf5031fbd51
tobitayuki <flying59@outlook.jp>
2020-12-02 21:16:34 +0900

    20201202 Commit

--- WebScraping/Program.cs
+++ WebScraping/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace WebScraping
 {
     class MainClass
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            WebScraping webScraping = new WebScraping("https://news.yahoo.co.jp/");
+            await webScraping.Scraping();
+            List<string> strList = webScraping.GetSelectTagText("b", true);
+            foreach (string s in strList)
+            {
+                Console.WriteLine("b:" + s);
+            }
+            strList = webScraping.GetSelectTagText("a", true);
+            foreach (string s in strList)
+            {
+                Console.WriteLine("a:" + s);
+            }
+            strList = webScraping.GetAncherURL();
+            foreach (string s in strList)
+            {
+                Console.WriteLine("href:" + s);
+            }
+            Console.WriteLine("Finish");
         }
     }
 }
---
+++ WebScraping/WebScraping.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebScraping
+{
+    public class WebScraping
+    {
+        private readonly string url;
+        public string HtmlText { set; get; }
+
+        public WebScraping(string _url)
+        {
+            url = _url;
+        }
+       
+        public async Task Scraping()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | (SecurityProtocolType)768 | (SecurityProtocolType)3072;
+            try
+            {
+                var client = new HttpClient();
+                var request = new HttpRequestMessage(HttpMethod.Get, @url);
+
+                var res = await client.SendAsync(request);
+                string HTMLText = await res.Content.ReadAsStringAsync();
+
+                HtmlText = RemoveStyle(RemoveScript(HTMLText));
+            }
+            catch
+            {
+                Console.WriteLine("ScrapingErr");
+            }
+        }
+
+        private string RemoveScript(string _str)
+        {
+            string str = _str;
+            MatchCollection matches = Regex.Matches(str, @"<script(.*?|\n)*>(.*?|\n)*</script>");
+            foreach (Match match in matches)
+            {
+                str = str.Replace(match.Value, "");
+            }
+            return str;
+        }
+
+        private string RemoveStyle(string _str)
+        {
+            string str = _str;
+            MatchCollection matches = Regex.Matches(str, @"<style(.*?|\n)*>(.*?|\n)*</style>");
+            foreach (Match match in matches)
+            {
+                str = str.Replace(match.Value, "");
+            }
+            return str;
+        }
+
+        public List<string> GetSelectTagText(string tag, bool noTag)
+        {
+            List<string> list = new List<string>();
+            string key = "<" + tag + @"(\s|>)(.*?|\n|>)*(.*?|\n)*</" + tag + @">";
+            MatchCollection matches = Regex.Matches(HtmlText, @key);
+            foreach (Match match in matches)
+            {
+                if (noTag)
+                {
+                    string textStr = match.Value.Replace("</" + tag + ">", "");
+                    list.Add(Regex.Replace(textStr, @"<(.*?)>", ""));
+                }
+                else
+                {
+                    list.Add(match.Value);
+                }
+            }
+            return list;
+        }
+
+        public List<string> GetAncherURL()
+        {
+            List<string> list = new List<string>();
+            string key = @"<a(\s|>)(.*?|\n|>)*(.*?|\n)*</a>";
+            MatchCollection matches = Regex.Matches(HtmlText, @key);
+            foreach (Match match in matches)
+            {
+                if (!Regex.Match(match.Value, @"rel=nofollow").Success)
+                {
+                    Match href = Regex.Match(match.Value, @"href=""(.*?)""");
+                    string hrefText = href.Value.Replace("\"", "");
+                    list.Add(hrefText.Replace("href=", ""));
+                }
+            }
+            return list;
+        }
+    }
+}
